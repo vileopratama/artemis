d891cf31dac7389cd793443a25a9739874498bd9
suhe <hendarsyahss@gmail.com>
2017-02-13 17:35:42 +0700

    Update

--- extensions/united_backend_theme/static/src/js/toggle_leftbar.js
+++ extensions/united_backend_theme/static/src/js/toggle_leftbar.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
-    "use strict";
     $(".toggle_leftmenu").click(function() {
-            $(".o_sub_menu").animate({
+            $(".oe_leftbar").animate({
             width: 'toggle'
         }, 0);
     });

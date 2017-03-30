cc7fbdf08beebdbf0fe60b157d9f68b051eb41c9
suhe <hendarsyahss@gmail.com>
2017-03-30 20:49:04 +0800

    Update Partner

--- extensions/bdo_project/models/bdo_project_invoice.py
+++ extensions/bdo_project/models/bdo_project_invoice.py
@@ -3,20 +3,20 @@ from odoo import models, fields
 
 
 class ProjectInvoice(models.Model):
-	_name = 'bdo.project.invoice'
-	_inherit = 'bdo.project.invoice'
-	
-	tax_invoice = fields.Char(string='Tax Invoice Number')
-	tax_invoice_file = fields.Char(string='File')
-	tax_invoice_attachment = fields.Binary(string='Attachment')
-	date_payment_actual = fields.Date(string='Actual payment date')
-	
-	def action_set_paid(self, data):
-		args = {
-			'state': 'paid',
-			'date_payment_actual': data.get('date_actual', fields.Date.today()),
-			'tax_invoice': data.get('tax_invoice_number', ''),
-			'tax_invoice_file': data.get('tax_invoice_file', ''),
-			'tax_invoice_attachment': data.get('tax_invoice_attachment', ''),
-		}
-		self.write(args)
+    _name = 'bdo.project.invoice'
+    _inherit = 'bdo.project.invoice'
+
+    tax_invoice = fields.Char(string='Tax Invoice Number')
+    tax_invoice_file = fields.Char(string='File')
+    tax_invoice_attachment = fields.Binary(string='Attachment')
+    date_payment_actual = fields.Date(string='Actual payment date')
+
+    def action_set_paid(self, data):
+        args = {
+            'state': 'paid',
+            'date_payment_actual': data.get('date_actual', fields.Date.today()),
+            'tax_invoice': data.get('tax_invoice_number', ''),
+            'tax_invoice_file': data.get('tax_invoice_file', ''),
+            'tax_invoice_attachment': data.get('tax_invoice_attachment', ''),
+        }
+        self.write(args)
--- extensions/bdo_project/models/res_partner.py
+++ extensions/bdo_project/models/res_partner.py
@@ -1,14 +1,22 @@
 # -*- coding: utf-8 -*-
 from odoo import api, fields, models
 
+
 class ResPartner(models.Model):
-	_inherit = 'res.partner'
-	
-	project_count = fields.Integer("Project", compute='_compute_project_count')
-	
-	@api.multi
-	def _compute_project_count(self):
-		for partner in self:
-			operator = 'child_of' if partner.is_company else '='
-			partner.project_count = self.env['bdo.project'].search_count(
-				[('partner_id', operator, partner.id)])
+    _inherit = 'res.partner'
+
+    project_count = fields.Integer("Project", compute='_compute_project_count')
+    invoice_count = fields.Integer("Invoice", compute='_compute_invoice_count')
+
+    @api.multi
+    def _compute_project_count(self):
+        for partner in self:
+            operator = 'child_of' if partner.is_company else '='
+            partner.project_count = self.env['bdo.project'].search_count(
+                [('partner_id', operator, partner.id)])
+
+    @api.multi
+    def _compute_invoice_count(self):
+        for partner in self:
+            partner.invoice_count = self.env['bdo.project.invoice'].search_count(
+                [('partner_id', '=', partner.name)])

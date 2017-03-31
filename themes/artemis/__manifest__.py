6177032b41dc312257c566b424117c1fbab06396
suhe <hendarsyahss@gmail.com>
2017-03-31 17:18:59 +0700

    Add Page Install

---
+++ themes/artemis/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+{
+	
+	'name': 'Artemis theme',
+	'description': 'Artemis Modern Website Theme',
+	'version': '10.1.0',
+	'author': 'Suhendar',
+	'data': [
+		'views/website_layout.xml',
+	],
+	'category': 'Theme/Creative',
+	'depends': ['website'],
+}

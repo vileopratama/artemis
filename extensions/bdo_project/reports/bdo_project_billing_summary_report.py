1c6596e77bfa9664e24523d4b1fbeb66d0ae6588
suhe <hendarsyahss@gmail.com>
2017-04-11 10:32:44 +0700

    Summary Report

--- extensions/bdo_project/reports/bdo_project_billing_summary_report.py
+++ extensions/bdo_project/reports/bdo_project_billing_summary_report.py
@@ -9,30 +9,42 @@ class ReportBillingSummary(models.Model):
 	partner_id = fields.Many2one(comodel_name='res.partner',string='Client')
 	service_id = fields.Many2one(comodel_name='bdo.project.service',string='Service')
 	year_invoice = fields.Char(string='Year',size=8)
-	jan = fields.Integer(string='Jan',default=0,size=1)
-	jan_paid = fields.Integer(string='Jan Paid',default=0,size=1)
-	feb = fields.Integer(string='Feb',default=0,size=1)
-	feb_paid = fields.Integer(string='Feb Paid',default=0,size=1)
-	mar = fields.Integer(string='Mar',default=0,size=1)
-	mar_paid = fields.Integer(string='Mar Paid',default=0,size=1)
-	apr = fields.Integer(string='Apr',default=0,size=1)
-	apr_paid = fields.Integer(string='Apr Paid',default=0,size=1)
-	may = fields.Integer(string='May',default=0,size=1)
-	may_paid = fields.Integer(string='May Paid',default=0,size=1)
-	jun = fields.Integer(string='Jun',default=0,size=1)
-	jun_paid = fields.Integer(string='Jun',default=0,size=1)
-	jul = fields.Integer(string='Jul',default=0,size=1)
-	jul_paid = fields.Integer(string='Jul Paid',default=0,size=1)
-	aug = fields.Integer(string='Aug',default=0,size=1)
-	aug_paid = fields.Integer(string='Aug Paid',default=0,size=1)
-	sept = fields.Integer(string='Sept',default=0,size=1)
-	sept_paid = fields.Integer(string='Sept Paid',default=0,size=1)
-	oct = fields.Integer(string='Oct',default=0,size=1)
-	oct_paid = fields.Integer(string='Oct',default=0,size=1)
-	nov = fields.Integer(string='Nov',default=0,size=1)
-	nov_paid = fields.Integer(string='Nov Paid',default=0,size=1)
-	dec = fields.Integer(string='Dec',default=0,size=1)
-	dec_paid = fields.Integer(string='Dec Paid',default=0,size=1)
+	jan = fields.Integer(string='Jan',default=0,size=2)
+	jan_paid = fields.Integer(string='Jan Paid',default=0,size=2)
+	jan_aging = fields.Integer(string='Jan Aging', default=0, size=3)
+	feb = fields.Integer(string='Feb',default=0,size=2)
+	feb_paid = fields.Integer(string='Feb Paid',default=0,size=2)
+	feb_aging = fields.Integer(string='Feb Aging', default=0, size=3)
+	mar = fields.Integer(string='Mar',default=0,size=2)
+	mar_paid = fields.Integer(string='Mar Paid',default=0,size=2)
+	mar_aging = fields.Integer(string='Mar Aging', default=0, size=3)
+	apr = fields.Integer(string='Apr',default=0,size=2)
+	apr_paid = fields.Integer(string='Apr Paid',default=0,size=2)
+	apr_aging= fields.Integer(string='Apr Aging', default=0, size=3)
+	may = fields.Integer(string='May',default=0,size=2)
+	may_paid = fields.Integer(string='May Paid',default=0,size=2)
+	may_aging = fields.Integer(string='May Aging', default=0, size=3)
+	jun = fields.Integer(string='Jun',default=0,size=2)
+	jun_paid = fields.Integer(string='Jun Paid',default=0,size=2)
+	jun_paid = fields.Integer(string='Jun Aging', default=0, size=3)
+	jul = fields.Integer(string='Jul',default=0,size=2)
+	jul_paid = fields.Integer(string='Jul Paid',default=0,size=2)
+	jul_aging = fields.Integer(string='Jul Aging', default=0, size=3)
+	aug = fields.Integer(string='Aug',default=0,size=2)
+	aug_paid = fields.Integer(string='Aug Paid',default=0,size=2)
+	aug_aging = fields.Integer(string='Aug Aging', default=0, size=3)
+	sept = fields.Integer(string='Sept',default=0,size=2)
+	sept_paid = fields.Integer(string='Sept Paid',default=0,size=2)
+	sept_aging = fields.Integer(string='Sept Aging', default=0, size=3)
+	oct = fields.Integer(string='Oct',default=0,size=2)
+	oct_paid = fields.Integer(string='Oct Paid',default=0,size=2)
+	oct_aging = fields.Integer(string='Oct Aging', default=0, size=3)
+	nov = fields.Integer(string='Nov',default=0,size=2)
+	nov_paid = fields.Integer(string='Nov Paid',default=0,size=2)
+	nov_aging = fields.Integer(string='Nov Aging', default=0, size=3)
+	dec = fields.Integer(string='Dec',default=0,size=2)
+	dec_paid = fields.Integer(string='Dec Paid',default=0,size=2)
+	dec_aging = fields.Integer(string='Dec Aging', default=0, size=3)
 	
 	@api.model_cr
 	def init(self):
@@ -46,28 +58,52 @@ class ReportBillingSummary(models.Model):
 					EXTRACT(YEAR FROM bpi.date_invoice) as year_invoice,
 					SUM(CASE WHEN bpi.jan_period<>0 THEN EXTRACT(MONTH FROM bpi.date_invoice) ELSE 0 END) AS jan,
                     SUM(CASE WHEN bpi.jan_period<>0 AND bpi.state='paid' THEN 1 ELSE 0 END) AS jan_paid,
+                    SUM(CASE WHEN bpi.jan_period<>0 AND bpi.state<>'paid' THEN DATE_PART('day', now() - bpi.date_invoice) ELSE 0 END) AS jan_aging,
+                    
                     SUM(CASE WHEN bpi.feb_period<>0 THEN EXTRACT(MONTH FROM bpi.date_invoice) ELSE 0 END) AS feb,
                     SUM(CASE WHEN bpi.feb_period<>0 AND bpi.state='paid' THEN 1 ELSE 0 END) AS feb_paid,
+                    SUM(CASE WHEN bpi.feb_period<>0 AND bpi.state<>'paid' THEN DATE_PART('day', now() - bpi.date_invoice) ELSE 0 END) AS feb_aging,
+                    
 					SUM(CASE WHEN bpi.mar_period<>0 THEN EXTRACT(MONTH FROM bpi.date_invoice) ELSE 0 END) AS mar,
                     SUM(CASE WHEN bpi.mar_period<>0 AND bpi.state='paid 'THEN 1 ELSE 0 END) AS mar_paid,
+                    SUM(CASE WHEN bpi.mar_period<>0 AND bpi.state<>'paid' THEN DATE_PART('day', now() - bpi.date_invoice) ELSE 0 END) AS mar_aging,
+                    
                     SUM(CASE WHEN bpi.apr_period<>0 THEN EXTRACT(MONTH FROM bpi.date_invoice) ELSE 0 END) AS apr,
                     SUM(CASE WHEN bpi.apr_period<>0 AND bpi.state='paid' THEN 1 ELSE 0 END) AS apr_paid,
+                    SUM(CASE WHEN bpi.apr_period<>0 AND bpi.state<>'paid' THEN DATE_PART('day', now() - bpi.date_invoice) ELSE 0 END) AS apr_aging,
+                    
                     SUM(CASE WHEN bpi.may_period<>0 THEN EXTRACT(MONTH FROM bpi.date_invoice) ELSE 0 END) AS may,
                     SUM(CASE WHEN bpi.may_period<>0 AND bpi.state='paid' THEN 1 ELSE 0 END) AS may_paid,
+                    SUM(CASE WHEN bpi.may_period<>0 AND bpi.state<>'paid' THEN DATE_PART('day', now() - bpi.date_invoice) ELSE 0 END) AS may_aging,
+                    
                     SUM(CASE WHEN bpi.jun_period<>0 THEN EXTRACT(MONTH FROM bpi.date_invoice) ELSE 0 END) AS jun,
                     SUM(CASE WHEN bpi.jun_period<>0 AND bpi.state='paid' THEN 1 ELSE 0 END) AS jun_paid,
+                    SUM(CASE WHEN bpi.jun_period<>0 AND bpi.state<>'paid' THEN DATE_PART('day', now() - bpi.date_invoice) ELSE 0 END) AS jun_aging,
+                    
                     SUM(CASE WHEN bpi.jul_period<>0 THEN EXTRACT(MONTH FROM bpi.date_invoice) ELSE 0 END) AS jul,
                     SUM(CASE WHEN bpi.jul_period<>0 AND bpi.state='paid' THEN 1 ELSE 0 END) AS jul_paid,
+                    SUM(CASE WHEN bpi.jul_period<>0 AND bpi.state<>'paid' THEN DATE_PART('day', now() - bpi.date_invoice) ELSE 0 END) AS jul_aging,
+                    
                     SUM(CASE WHEN bpi.aug_period<>0 THEN EXTRACT(MONTH FROM bpi.date_invoice) ELSE 0 END) AS aug,
                     SUM(CASE WHEN bpi.aug_period<>0 AND bpi.state='paid' THEN 1 ELSE 0 END) AS aug_paid,
+                    SUM(CASE WHEN bpi.aug_period<>0 AND bpi.state<>'paid' THEN DATE_PART('day', now() - bpi.date_invoice) ELSE 0 END) AS aug_aging,
+                    
                     SUM(CASE WHEN bpi.sept_period<>0 THEN EXTRACT(MONTH FROM bpi.date_invoice) ELSE 0 END) AS sept,
                     SUM(CASE WHEN bpi.sept_period<>0 AND bpi.state='paid' THEN 1 ELSE 0 END) AS sept_paid,
+                    SUM(CASE WHEN bpi.sept_period<>0 AND bpi.state<>'paid' THEN DATE_PART('day', now() - bpi.date_invoice) ELSE 0 END) AS sept_aging,
+                    
                     SUM(CASE WHEN bpi.oct_period<>0 THEN EXTRACT(MONTH FROM bpi.date_invoice) ELSE 0 END) AS oct,
                     SUM(CASE WHEN bpi.oct_period<>0 AND bpi.state='paid' THEN 1 ELSE 0 END) AS oct_paid,
+                    SUM(CASE WHEN bpi.oct_period<>0 AND bpi.state<>'paid' THEN DATE_PART('day', now() - bpi.date_invoice) ELSE 0 END) AS oct_aging,
+                    
                     SUM(CASE WHEN bpi.nov_period<>0 THEN EXTRACT(MONTH FROM bpi.date_invoice) ELSE 0 END) AS nov,
                     SUM(CASE WHEN bpi.nov_period<>0 AND bpi.state='paid' THEN 1 ELSE 0 END) AS nov_paid,
+                    SUM(CASE WHEN bpi.nov_period<>0 AND bpi.state<>'paid' THEN DATE_PART('day', now() - bpi.date_invoice) ELSE 0 END) AS nov_aging,
+                    
                     SUM(CASE WHEN bpi.dec_period<>0 THEN EXTRACT(MONTH FROM bpi.date_invoice) ELSE 0 END) AS dec,
-                    SUM(CASE WHEN bpi.dec_period<>0 AND bpi.state='paid' THEN 1 ELSE 0 END) AS dec_paid
+                    SUM(CASE WHEN bpi.dec_period<>0 AND bpi.state='paid' THEN 1 ELSE 0 END) AS dec_paid,
+					SUM(CASE WHEN bpi.dec_period<>0 AND bpi.state<>'paid' THEN DATE_PART('day', now() - bpi.date_invoice) ELSE 0 END) AS dec_aging
+					
 				FROM
 					bdo_project_invoice AS bpi
 				INNER JOIN
--- extensions/bdo_project/static/src/js/billing_widget.js
+++ extensions/bdo_project/static/src/js/billing_widget.js
@@ -36,30 +36,43 @@ return Widget.extend({
 				                year_invoice: record.year_invoice,
 								jan : self.convert_month_name(record.jan),
 								jan_paid : record.jan_paid,
+								jan_aging : self.convert_class_attribute(record.jan_aging),
 								feb : self.convert_month_name(record.feb),
 								feb_paid : record.feb_paid,
+								feb_aging : self.convert_class_attribute(record.feb_aging),
 								mar : self.convert_month_name(record.mar),
 								mar_paid : record.mar_paid,
+								mar_aging : self.convert_class_attribute(record.mar_aging),
 								apr : self.convert_month_name(record.apr),
 								apr_paid : record.apr_paid,
+								apr_aging : self.convert_class_attribute(record.apr_aging),
 								may : self.convert_month_name(record.may),
 								may_paid : record.may_paid,
+								may_aging : self.convert_class_attribute(record.may_aging),
 								jun : self.convert_month_name(record.jun),
 								jun_paid : record.jun_paid,
+								jun_aging : self.convert_class_attribute(record.jun_aging),
 								jul : self.convert_month_name(record.jul),
 								jul_paid : record.jul_paid,
+								jul_aging : self.convert_class_attribute(record.jul_aging),
 								aug : self.convert_month_name(record.aug),
 								aug_paid : record.aug_paid,
+								aug_aging : self.convert_class_attribute(record.aug_aging),
 								sept : self.convert_month_name(record.sept),
 								sept_paid : record.sept_paid,
+								sept_aging : self.convert_class_attribute(record.sept_aging),
 								oct : self.convert_month_name(record.oct),
 								oct_paid : record.oct_paid,
+								oct_aging : self.convert_class_attribute(record.oct_aging),
 								nov : self.convert_month_name(record.nov),
 								nov_paid : record.nov_paid,
+								nov_aging : self.convert_class_attribute(record.nov_aging),
 								dec : self.convert_month_name(record.dec),
-								dec_paid : record.dec_paid
+								dec_paid : record.dec_paid,
+								dec_aging : self.convert_class_attribute(record.dec_aging)
+
 				            });
-				             console.log('Year :' + record.year_invoice + ' partner : ' + record.partner_id);
+				             //console.log('Year :' + record.year_invoice + ' partner : ' + record.partner_id);
 				         });
                     });
     },
@@ -107,6 +120,24 @@ return Widget.extend({
 				break;
 		}
 		return result;	
+	},
+	convert_class_attribute: function(aging) {
+		var result;
+		if(aging >= 1 && aging <= 30) {
+			result = 'on_before_30';
+		}else if(aging >= 31 && aging <= 60) {
+			result = 'on_before_60';
+		}else if(aging >= 61 && aging <= 90) {
+			result = 'on_before_90';
+		}else if(aging >= 91 && aging <= 120) {
+			result = 'on_before_120';
+		}else if(aging >= 121) {
+			result = 'on_after_120';
+		}  else {
+			result = '';
+		}
+
+		return result;
 	},
     update_data: function (domain, groupbys) {
         this.domain = domain;
